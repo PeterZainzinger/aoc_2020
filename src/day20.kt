b0244f1859b72e66b8d9cf75b60d48fce277abed
Peter Zainzinger <peterzainzinger@gmail.com>
2020-12-20 20:16:34 +0100

    a bit of cleanup

--- src/day20.kt
+++ src/day20.kt
@@ -1,5 +1,4 @@
 import java.io.File
-import kotlin.math.ceil
 import kotlin.math.floor
 import kotlin.math.sqrt
 
@@ -10,8 +9,7 @@ sealed class Pixel(val item: Char) {
 
 data class Tile(val id: Int, val pixels: List<List<Pixel>>) {
     override fun toString() =
-        "\nTile $id:\n" + pixels.map { it.joinToString(separator = "") { it.item.toString() } }
-            .joinToString("\n") + "\n\n"
+        "\nTile $id:\n" + pixels.joinToString("\n") { it.joinToString(separator = "") { it.item.toString() } } + "\n\n"
 
     fun alignsToRight(right: Tile) = getRightEdge() == right.getLeftEdge()
     fun alignsToBottom(bottom: Tile) = getBottomEdge() == bottom.getTopEdge()
@@ -21,12 +19,12 @@ data class Tile(val id: Int, val pixels: List<List<Pixel>>) {
         else -> null
     }
 
-    fun allOptions(): List<Tile> {
+    val allOptions by lazy {
         val rot0 = this
         val rot1 = rotate()
         val rot2 = rot1.rotate()
         val rot3 = rot2.rotate()
-        return listOf(rot0, rot1, rot2, rot3).flatMap { source ->
+        listOf(rot0, rot1, rot2, rot3).flatMap { source ->
             listOf(source, source.flipX(), source.flipY(), source.flipX().flipY())
         }.toSet().toList()
     }
@@ -62,7 +60,6 @@ data class Tile(val id: Int, val pixels: List<List<Pixel>>) {
     fun contentLines() = pixels.subList(1, pixels.size - 1).map { it.subList(1, it.size - 1) }.map {
         it.map { it.item }.joinToString("")
     }
-
 }
 
 private fun parseTile(input: List<String>) = input.map {
@@ -82,7 +79,6 @@ fun day20() {
         val pixels = parseTile(lines.subList(1, lines.size))
         Tile(id, pixels)
     }
-    val targetFile = tiles.first()
     val width = sqrt(tiles.size.toDouble()).toInt()
     val startState = SearchState(
         filled = emptyMap(),
@@ -91,10 +87,8 @@ fun day20() {
     )
     var currentStates = listOf(startState)
     tiles.indices.forEach { loop ->
-        println("current loop $loop")
-        currentStates = currentStates.flatMap { searchForNext(it) ?: emptyList() }
+        currentStates = currentStates.flatMap { searchForNext(it) }
     }
-    println(currentStates.size)
     val oneOption = currentStates.first()
     val res = listOf(
         oneOption.filled[GridPosition(0, 0)]!!.id,
@@ -103,10 +97,8 @@ fun day20() {
         oneOption.filled[GridPosition(width - 1, width - 1)]!!.id,
     ).fold(1L, { a, b -> a * b })
     println(res)
-
     var picture = ""
     for (lineIndex in 0 until width) {
-
         val linesPerColumn = (0 until width).map { oneOption.filled[GridPosition(it, lineIndex)]!!.contentLines() }
         val innerLength = linesPerColumn.first().size
         for (innerIndex in 0 until innerLength) {
@@ -126,10 +118,8 @@ fun day20() {
         }.filterNotNull()
 
     }.flatten()
-    println(monsterPattern)
-    println(monsterIndices)
 
-    pictureTile.allOptions().forEach { picture ->
+    pictureTile.allOptions.forEach { picture ->
         var count = 0
         for (i in picture.pixels.indices) {
             for (j in picture.pixels.indices) {
@@ -148,20 +138,16 @@ fun day20() {
 }
 
 
-fun searchForNext(state: SearchState): List<SearchState>? {
+fun searchForNext(state: SearchState): List<SearchState> {
     val nextIndex = state.filled.size
     val x = nextIndex % state.width
     val y = floor(nextIndex / state.width.toDouble()).toInt()
-    val allOptions = state.remainingOptions.flatMap { it.allOptions() }
+    val allOptions = state.remainingOptions.flatMap { it.allOptions }
     val filtered = allOptions
         // if top matches
         .filter { opt -> y == 0 || state.filled[GridPosition(x, y - 1)]!!.alignsToBottom(opt) }
         // if left matches
         .filter { opt -> x == 0 || state.filled[GridPosition(x - 1, y)]!!.alignsToRight(opt) }
-
-    if (filtered.isEmpty()) {
-        return null
-    }
 
     return filtered.map { taken ->
         state.copy(
